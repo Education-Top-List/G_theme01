bb4fa4928d0146d73953946d7dd15262b56d9afd
mocslop <mocslop@example.com>
2018-09-25 23:01:33 +0700

    push code 25/9

--- wp-content/themes/doanhnghiep/page.php
+++ wp-content/themes/doanhnghiep/page.php
@@ -5,8 +5,24 @@ if(have_posts()) :
 	while (have_posts()) : the_post() ; ?>
 		<div class="container">
 			<article class="post page">
-				<h2><?php the_title(); ?></a></h2>
-				<?php the_content(); ?>
+				<?php if(has_children() OR $post->post_parent>0) { ?>
+
+
+				<nav class="short_nav">
+					<span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
+					<ul>
+						<?php $args = array(
+							'child_of' => get_top_ancestor_id(),
+							'title_li' => ''
+						)
+						?>
+						<?php wp_list_pages($args);?>
+				
+					</ul>
+				</nav>
+			<?php } ?> 
+					<h2><?php the_title(); ?></a></h2>
+					<?php the_content(); ?>
 			</article>
 		</div>  <!-- container -->
 	<?php endwhile;
