39a3c62029788958f263578e3b16f97d963f33c2
quanvt <quanvt@ominext>
2018-10-11 14:13:54 +0700

    updater.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Thiết lập MySQL - Bạn có thể lấy các thông tin này từ host/server ** //
 /** Tên database MySQL */
-define('DB_NAME', 'yang');
+define('DB_NAME', 'gtheme01');
 
 /** Username của database */
 define('DB_USER', 'root');
