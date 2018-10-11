e310aae0cc408725b45e3ff0a57504a77c99907d
quanvt <quanvt@ominext>
2018-10-11 14:24:22 +0700

    fix db

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Thiết lập MySQL - Bạn có thể lấy các thông tin này từ host/server ** //
 /** Tên database MySQL */
-define('DB_NAME', 'gtheme01');
+define('DB_NAME', 'yang');
 
 /** Username của database */
 define('DB_USER', 'root');
